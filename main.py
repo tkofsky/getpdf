818dbef0a7a0c3aa9163aa4552e96841f54747e6
tkofsky <kofsky.tev@gmail.com>
2022-08-16 14:24:27 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+import requests
+import csv
+import time
+import os
+import fitz
+countx=0
+from pathlib import Path
+cnt=0
+zerosize =0
+with open('xml apps.dat', encoding='latin-1') as f:  #in
+
+    sreader=csv.reader(f, delimiter=' ', quotechar='"')
+    for row in sreader:
+        countx+=1
+        time.sleep(5.1) #5.1 #15.1
+        sn=str(row[1])
+        dte=str(row[2])
+      #111112sffdsfsds s w
+
+        dte=dte[0:4]+"-"+dte[4:6]+"-"+dte[6:8]
+        url = "https://tsdrapi.uspto.gov/ts/cd/casedocs/bundle.pdf?sn="+sn+"&date="+dte+"&USPTO-API-KEY=p0U59nBx9u2WE0tFzednzmHXv9NbkThe"
+        #url = "https://tsdrapi.uspto.gov/ts/cd/casedocs/bundle.pdf?sn=90815289&date=20220326&USPTO-API-KEY=1csin1EnVbBiVYlYqhxPZLThC9s4LjwW"
+        #url = "https://tsdrapi.uspto.gov/ts/cd/casedocs/bundle.pdf?sn=90813837&date=2022-03-24&USPTO-API-KEY=1csin1EnVbBiVYlYqhxPZLThC9s4LjwW"
+
+        payload = {}
+        headers = {
+            'USPTO-API-KEY': 'p0U59nBx9u2WE0tFzednzmHXv9NbkThe',
+            'Cookie': 'TS01b2ceaa=01874167c7092c3ccdc9e0fb4fbc51a8fb587127a556b024f727d3c426d9a4333cbb86fd42ea9318b6b10a86c145f4164d0f7f8b30'
+        }
+#serwererererwrwrerrwr
+#imer#u r
+#1Jlxj8XsmbqWXEJyLE1X2CaejNK2qIct
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        print (response)
+        print (sn)
+
+        if response.status_code!=404:
+            filename = sn+".pdf"
+            htmldir=Path("files/")
+
+            pdffiletouse = htmldir / filename
+            with open(pdffiletouse, "wb") as f:
+                f.write(response.content)### write pdf to file
+
+            filename = sn + ".pdf"
+            htmldir = Path("files/")
+            pdffiletouse = htmldir / filename
+            file=Path(pdffiletouse)
+            if file.exists() and file.stat().st_size > 20000: #1: # check if size is > 200000 bytes and it downloaded correctly
+                with fitz.open(pdffiletouse) as doc: ## open PDF
+                    text = ""
+                    for page in doc:
+                        text += page.getText().strip()
+                    #print (text)
+                    filename = sn + ".txt"
+                    htmldir = Path("files/")
+                    txtfiletouse = htmldir / filename
+                    with open(txtfiletouse,"w", encoding="utf-8") as f:## write as TXT file
+                        f.write(text)
+            else: # save to look at later
+                zerosize+=1
+                htmldir = Path("files/")
+                filetouse = Path(htmldir / 'snerror.txt')
+                dte = dte.replace("-", "")
+                text = "OOA " + sn + " " + dte+" not complete"
+                with open(filetouse, "a", encoding="utf-8") as f:  ## write as TXT file
+                    f.write(text)
+                    f.write("\n")
+        else:  ### if 404 then write to file for later
+            cnt+=1
+            print (response.status_code)
+            htmldir = Path("files/")
+            filetouse = Path(htmldir/'snerror.txt')
+            dte = dte.replace("-","")
+            text = "OOA "+sn+" "+dte
+            with open(filetouse, "a", encoding="utf-8") as f:  ## write as TXT file
+                f.write(text)
+                f.write("\n")
+
+print ("***************")
+print (cnt)
+print (zerosize)
