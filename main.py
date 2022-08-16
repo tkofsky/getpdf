3194f0e7f72366906e5791f46b5f7f90ad1f8726
tkofsky <kofsky.tev@gmail.com>
2022-08-16 17:08:54 -0400

    added toggle

--- main.py
+++ main.py
@@ -7,26 +7,41 @@ countx=0
 from pathlib import Path
 cnt=0
 zerosize =0
+toggle=0
 with open('xml apps.dat', encoding='latin-1') as f:  #in
 
     sreader=csv.reader(f, delimiter=' ', quotechar='"')
     for row in sreader:
         countx+=1
-        time.sleep(5.1) #5.1 #15.1
+        time.sleep(8.0) #5.1 #15.1
         sn=str(row[1])
         dte=str(row[2])
       #111112sffdsfsds s w
 #ewrwerwe 225 5555 ewrwerwe comm2
         dte=dte[0:4]+"-"+dte[4:6]+"-"+dte[6:8]
-        url = "https://tsdrapi.uspto.gov/ts/cd/casedocs/bundle.pdf?sn="+sn+"&date="+dte+"&USPTO-API-KEY=p0U59nBx9u2WE0tFzednzmHXv9NbkThe"
+
+        if toggle==1:
+            print ("toggle1")
+            url = "https://tsdrapi.uspto.gov/ts/cd/casedocs/bundle.pdf?sn="+sn+"&date="+dte+"&USPTO-API-KEY=p0U59nBx9u2WE0tFzednzmHXv9NbkThe"
         #url = "https://tsdrapi.uspto.gov/ts/cd/casedocs/bundle.pdf?sn=90815289&date=20220326&USPTO-API-KEY=1csin1EnVbBiVYlYqhxPZLThC9s4LjwW"
         #url = "https://tsdrapi.uspto.gov/ts/cd/casedocs/bundle.pdf?sn=90813837&date=2022-03-24&USPTO-API-KEY=1csin1EnVbBiVYlYqhxPZLThC9s4LjwW"
 
-        payload = {}
-        headers = {
+            payload = {}
+            headers = {
             'USPTO-API-KEY': 'p0U59nBx9u2WE0tFzednzmHXv9NbkThe',
             'Cookie': 'TS01b2ceaa=01874167c7092c3ccdc9e0fb4fbc51a8fb587127a556b024f727d3c426d9a4333cbb86fd42ea9318b6b10a86c145f4164d0f7f8b30'
         }
+            toggle=0
+        else:
+            print("toggle0")
+            url = "https://tsdrapi.uspto.gov/ts/cd/casedocs/bundle.pdf?sn=" + sn + "&date=" + dte + "&USPTO-API-KEY=hMtewl5gpVnSU5oCjGhffQJRPzzbuQa8"
+            payload = {}
+            headers = {
+                'USPTO-API-KEY': 'hMtewl5gpVnSU5oCjGhffQJRPzzbuQa8',
+                'Cookie': 'TS01b2ceaa=01874167c753dbf5751b58b7ffef05b2c7318a59da9beefbc678177cdad1e398c7629f0de3478f52406d8daa510e06accc0e58063c'
+            }
+            toggle=1
+
 #serwererererwrwrerrwr
 #imer#u r
 #1Jlxj8XsmbqWXEJyLE1X2CaejNK2qIct
