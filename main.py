94cfee4127d2cdf92ca8e484ec5ecf9a2801f7fe
tkofsky <kofsky.tev@gmail.com>
2022-08-21 17:22:47 -0400

    added next 2.51 next

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ with open('xml apps.dat', encoding='latin-1') as f:  #in
         time.sleep(8.0) #5.1 #15.1
         sn=str(row[1])
         dte=str(row[2])
-#234234242341
+#23423
        # sn="97124717"
        # dte="20220802"
         dte=dte[0:4]+"-"+dte[4:6]+"-"+dte[6:8]
